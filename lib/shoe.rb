ac744145e0486f5c79e314e0b14a14d85359956e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-29 17:20:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,14 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts ""
+    condition = "new"
+  end
   
 end
